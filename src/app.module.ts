646133ffd664d8a1aec0c812f9da803c0696d6a9
ali <ali.mir.developer@gmail.com>
2024-09-15 15:12:28 +0330

    addinf postgreSql and connect to it

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { PostModule } from './post/post.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, PostModule],
+  imports: [UsersModule, PostModule, TypeOrmModule.forRoot({
+    type : 'postgres',
+    entities : [],
+    synchronize : true ,
+    port:5432 , 
+    username : 'postgres',
+    password:'1',
+    host : 'localhost',
+    database:'nestjs-blog'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
